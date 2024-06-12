050df094df147239a14571ebda49b8f2fc4be971
Dylanve151 <50217470+Dylanve151@users.noreply.github.com>
2024-06-12 23:58:58 +0200

    Add files via upload

---
+++ updatemods/steamModsDownload.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+export IFS=";"
+A3gameID=107410
+A3ModsPath="/home/steam/Steam"
+
+cd /home/steam/steamcmd
+
+for modID in $SERVER_MODS; do
+    n=0
+    while [ $DOWNLOADRETRY -ge $n ]; do
+        if [ ! -d "${A3ModsPath}/steamapps/workshop/content/${A3gameID}/${modID}" ]; then
+            echo "Starting download ${modID}..."
+            ./steamcmd.sh +login $steamLogin +workshop_download_item $A3gameID $modID validate +quit | grep "Downloading item"
+        else
+            echo "${modID} Downloaded."
+            break
+        fi
+        n=$((n+1))
+    done
+done
+
+for modID in $OPTIONAL_MODS; do
+    n=0
+    while [ $DOWNLOADRETRY -ge $n ]; do
+        if [ ! -d "${A3ModsPath}/steamapps/workshop/content/${A3gameID}/${modID}" ]; then
+            echo "Starting download ${modID}..."
+            ./steamcmd.sh +login $steamLogin +workshop_download_item $A3gameID $modID validate +quit | grep "Downloading item"
+        else
+            echo "${modID} Downloaded."
+            break
+        fi
+        n=$((n+1))
+    done
+done
+
+for modID in $REQUIRED_MODS; do
+    n=0
+    while [ $DOWNLOADRETRY -ge $n ]; do
+        if [ ! -d "${A3ModsPath}/steamapps/workshop/content/${A3gameID}/${modID}" ]; then
+            echo "Starting download ${modID}..."
+            ./steamcmd.sh +login $steamLogin +workshop_download_item $A3gameID $modID validate +quit | grep "Downloading item"
+        else
+            echo "${modID} Downloaded."
+            break
+        fi
+        n=$((n+1))
+    done
+done
+
+unset IFS
